950452d5a18bb8df6e2b86fb0049a3513f6bc8c7
bharath122064 <bm8376@srmist.edu.in>
2026-04-09 00:11:04 +0530

    UC7

--- TrainApp/src/TrainConsistManagementApp.java
+++ TrainApp/src/TrainConsistManagementApp.java
@@ -1,18 +1,33 @@
 import java.util.*;
 
+class Bogie {
+    String name;
+    int capacity;
+
+    public Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String toString() {
+        return name + " -> " + capacity;
+    }
+}
+
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
-        HashMap<String, Integer> bogieCapacity = new HashMap<>();
+        List<Bogie> bogies = new ArrayList<>();
 
-        bogieCapacity.put("Sleeper", 72);
-        bogieCapacity.put("AC Chair", 60);
-        bogieCapacity.put("First Class", 40);
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 40));
 
-        System.out.println("Bogie Capacity Details:");
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
 
-        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        System.out.println("Sorted Bogies by Capacity:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
         }
     }
 }
