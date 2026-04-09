e516bd8cf094ee5502d0df37a9b7324f4eeec53d
bharath122064 <bm8376@srmist.edu.in>
2026-04-09 08:26:54 +0530

    UC17

--- TrainApp/src/TrainConsistManagementApp.java
+++ TrainApp/src/TrainConsistManagementApp.java
@@ -1,47 +1,20 @@
-class PassengerBogie {
-    String type;
-    int capacity;
-
-    public PassengerBogie(String type, int capacity) {
-        this.type = type;
-        this.capacity = capacity;
-    }
-
-    public void display() {
-        System.out.println(type + " - Capacity: " + capacity);
-    }
-}
+import java.util.Arrays;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
-        PassengerBogie[] bogies = {
-                new PassengerBogie("Sleeper", 72),
-                new PassengerBogie("AC Chair", 50),
-                new PassengerBogie("First Class", 30),
-                new PassengerBogie("Sleeper", 60)
-        };
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "Cylindrical", "Rectangular"};
 
         System.out.println("Before Sorting:");
-        for (PassengerBogie b : bogies) {
-            b.display();
+        for (String name : bogieNames) {
+            System.out.println(name);
         }
 
-        int n = bogies.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (bogies[j].capacity > bogies[j + 1].capacity) {
-                    PassengerBogie temp = bogies[j];
-                    bogies[j] = bogies[j + 1];
-                    bogies[j + 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(bogieNames);
 
         System.out.println("\nAfter Sorting:");
-        for (PassengerBogie b : bogies) {
-            b.display();
+        for (String name : bogieNames) {
+            System.out.println(name);
         }
     }
 }
